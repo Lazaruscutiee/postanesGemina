6b50dec01a90946fd889bd2e7438ce571b8705f8
GeminaCutie <143789822+Lazaruscutiee@users.noreply.github.com>
2024-10-01 19:13:55 +0800

    New

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -14,8 +14,29 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Kupal kaba?");
+       
+  
+        int a = 10;
+        int b = 5;
+        int c = 2;
+
+        int addition = a + b + c;
+        int subtraction = a - b - c;
+        int multiplication = a * b * c;
+
+        
+        double division;
+        if (b != 0 && c != 0) {
+            division = (double) a / b / c;
+        } else {
+            System.out.println("Division by zero error!");
+            division = 0;
+        }
+
+       
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
     }
-    
 }
